e52a2c7d79f349213ebdf639407be6f9e7f80f4b
Marissa <marbd11@gmail.com>
2017-04-23 05:29:26 -0400

    Added dropdown

--- popup.js
+++ popup.js
@@ -1,3 +1,25 @@
+function calculate() {
+  var grade = document.getElementById("gradeOp");
+  var strUser = grade.options[grade.selectedIndex].value;
+  if(strUser === 'A'){
+    gpaInd = 4.0
+  } else if (strUser === 'B+'){
+    gpaInd = 3.5;
+  } else if (strUser === 'B'){
+    gpaInd = 3.0;
+  } else if (strUser === 'C+'){
+    gpaInd = 2.5;
+  } else if (strUser === 'C'){
+    gpaInd = 2.0;
+  } else if (strUser === 'D'){
+    gpaInd = 1.0;
+  } else {
+    gpaInd = 0;
+  }
+  document.getElementById('creditName').innerHTML = gpaInd;
+}
+document.getElementById('do-submit').onclick = calculate;
+
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.action == "getSource") {
     document.getElementById('demo').textContent  = request.source;
